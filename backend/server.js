475db2d766ef4a6509abe12eff19b0ee8a68cbd3
Ashutosh Verma <ashutoshrgnict@gmail.com>
2024-05-07 09:42:42 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -11,6 +12,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
@@ -19,10 +22,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('/', (req, res) => {
-    // http://localhost:8000/
-    res.send('Hello World!');
+app.get('*', (req, res) => {
+    res.sendFile
+        (path.join(__dirname, '/frontend/dist/index.html'));
 });
 
 
